6b61b411ddb684ea96ad3357f6afc0a6d8662b8c
hmcgough <hugh.mcgough@wsu.edu>
2018-02-05 09:56:06 -0800

    initial commit

---
+++ t.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "type.h"
+
+PROC proc[NPROC], *readyQueue, *freeList;
+
+int main()
+{
+    int i;
+    PROC *p;
+    readyQueue = 0;
+
+    for (i=0; i < NPROC; i++){
+        p = &proc[i];
+        p->pid = i;
+        p->priority = rand() % 10;
+        printf("pid=%d priority=%d\n", p->pid, p->priority);
+        enqueue(&readyQueue, p);
+        printList("readyQ", readyQueue);
+    }
+}
