8804eb775b716a1a596c4c9593e196a5ff704646
hmcgough <hugh.mcgough@wsu.edu>
2018-02-14 23:46:48 -0800

    Add parsing for home and path from char *env[]
    path currently only prints out the path, it doesn't get saved yet.

--- sh.c
+++ sh.c
@@ -3,12 +3,35 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char *argv[]){
-    printf("YO\n");
-    for (int i = 0; i < argc; ++i) {
-        printf("%d: %s\n", i, argv[i]);
+char home[64];
+char curDirectory[128];
+
+int main(int argc, char *argv[], char *env[]){
+    for(int i = 0; env[i]; i++){
+        char envProp[strlen(env[i])+1];
+        strcpy(envProp, env[i]);
+        char *token = strtok(envProp, "=");
+        if(strcmp(token, "HOME") == 0){
+            strcpy(home, strtok(NULL, "\0"));
+        } else if (strcmp(token, "PATH") == 0){
+            printf("Path = %s\n", strtok(NULL, "\0"));
+        }
+//        envProp = strtok(env[i], "=");
+//        if(strcmp(envProp, "PATH") == 0){
+//            strcpy(path, env[i]);
+//            printf("%s", path);
+//        }
     }
 
-    printf("after the loop");
+    printf("HOME = %s\n", home);
+    strcpy(curDirectory, home);
+
+    char command[128] = "\0";
+
+    while(strcmp(command, "exit") != 0) {
+        printf("%s $ ", curDirectory);
+        scanf("%127s", command);
+    }
 }
