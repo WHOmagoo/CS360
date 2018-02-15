6f33baa526c8e1f3a29a48f5d040402ce0e6112e
hmcgough <hugh.mcgough@wsu.edu>
2018-02-14 22:36:10 -0800

    initial commit

--- sh.c
+++ sh.c
@@ -2,8 +2,13 @@
 // Created by whomagoo on 2/14/18.
 //
 
+#include <stdio.h>
+
 int main(int argc, const char *argv[]){
+    printf("YO\n");
     for (int i = 0; i < argc; ++i) {
         printf("%d: %s\n", i, argv[i]);
     }
+
+    printf("after the loop");
 }
