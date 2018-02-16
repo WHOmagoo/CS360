a9a981b53b637d89e2e2aa66e029ddccfcacfb9d
Hugh McGough <hugh.mcgough@wsu.edu>
2018-02-15 18:51:31 -0800

    Add testing for execve()

--- sh.c
+++ sh.c
@@ -4,11 +4,26 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 char home[64];
 char curDirectory[128];
 
+struct PathList *path;
+
+typedef struct PathList{
+    char path[64];
+    struct PathList *next;
+}PathList;
+
 int main(int argc, char *argv[], char *env[]){
+    int first, *myInt;
+    first ++;
+    int numbers[] = {1,2};
+
+    printf("%d", *myInt);
+
     for(int i = 0; env[i]; i++){
         char envProp[strlen(env[i])+1];
         strcpy(envProp, env[i]);
@@ -16,13 +31,32 @@ int main(int argc, char *argv[], char *env[]){
         if(strcmp(token, "HOME") == 0){
             strcpy(home, strtok(NULL, "\0"));
         } else if (strcmp(token, "PATH") == 0){
-            printf("Path = %s\n", strtok(NULL, "\0"));
+            token = strtok(NULL, ":");
+            int n = 0;
+            PathList *prev = 0;
+            while(token){
+                PathList *curPath = (PathList *) malloc(sizeof(PathList));
+                if(prev){
+                    prev->next = curPath;
+                } else {
+                    path = curPath;
+                }
+
+
+                strcpy(curPath->path, token);
+                curPath->next = 0;
+
+                prev = curPath;
+                token = strtok(NULL, ":");
+            }
         }
-//        envProp = strtok(env[i], "=");
-//        if(strcmp(envProp, "PATH") == 0){
-//            strcpy(path, env[i]);
-//            printf("%s", path);
-//        }
+    }
+
+    PathList *tmp = path;
+
+    while(tmp){
+        printf("%s\n", tmp->path);
+        tmp = tmp->next;
     }
 
     printf("HOME = %s\n", home);
@@ -33,5 +67,26 @@ int main(int argc, char *argv[], char *env[]){
     while(strcmp(command, "exit") != 0) {
         printf("%s $ ", curDirectory);
         scanf("%127s", command);
+        execute(command, env);
     }
+}
+
+int execute(char *program, char *env[]){
+    PathList *cur = path;
+    char *myargv[3] = {program,  "TestBoi", "\0"};
+
+    char *args[] = {"TestBoi", NULL};
+
+    execve("/usr/bin/mkdir", args, env);
+//    while(cur){
+//        int result = 0;
+//        result = execve(cur->path, myargv, env);
+//
+//        if(result == -1){
+//            printf("Not found in %s\n", cur->path);
+//        }
+//
+//        cur = cur->next;
+//
+//    }
 }
