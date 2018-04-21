aa3d1efbfb9f04a43f1ca22a3c2848634db47673
Hugh McGough <hugh.mcgough@wsu.edu>
2018-04-21 16:09:00 -0700

    Initial commit

---
+++ disk.c
@@ -0,0 +1,33 @@
+#include "disk.h"
+
+GD    *gp;
+SUPER *sp;
+INODE *ip;
+DIR   *dp;
+
+char gBuf[BLKSIZE];
+char sBuf[BLKSIZE];
+char iBuf[BLKSIZE];
+char dBuf[BLKSIZE];
+
+int get_block(int fd, int blk, char buf[ ])
+{
+    lseek(fd,(long)blk*BLKSIZE, 0);
+    read(fd, buf, BLKSIZE);
+}
+
+int init(int fd){
+    get_block(fd, 1, sBuf);
+    sp = (SUPER *) sBuf;
+
+    get_block(fd, 2, gBuf);
+    gp = (GD *) gBuf;
+}
+
+SUPER * getSp(){
+    return sp;
+}
+
+GD * getGd(){
+    return gp;
+}
---
+++ disk.h
@@ -0,0 +1,42 @@
+//
+// Created by whomagoo on 4/13/18.
+//
+
+#ifndef SHSIMULATOR_DISK_H
+#define SHSIMULATOR_DISK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <ext2fs/ext2_fs.h>
+
+#define BLKSIZE 1024
+
+typedef struct ext2_group_desc  GD;
+typedef struct ext2_super_block SUPER;
+typedef struct ext2_inode       INODE;
+typedef struct ext2_dir_entry_2 DIR;    // need this for new version of e2fs
+
+//struct ext2_super_block {
+//    u32  s_inodes_count;       // total number of inodes
+//    u32  s_blocks_count;       // total number of blocks
+//    u32  s_r_blocks_count;
+//    u32  s_free_blocks_count;  // current number of free blocks
+//    u32  s_free_inodes_count;  // current number of free inodes
+//    u32  s_first_data_block;   // first data block in this group
+//    u32  s_log_block_size;     // 0 for 1KB block size
+//    u32  s_log_frag_size;
+//    u32  s_blocks_per_group;   // 8192 blocks per group
+//    u32  s_frags_per_group;
+//    u32  s_inodes_per_group;
+//    u32  s_mtime;
+//    u32  s_wtime;
+//    u16  s_mnt_count;          // number of times mounted
+//    u16  s_max_mnt_count;      // mount limit
+//    u16  s_magic;              // 0xEF53
+
+int get_block(int fd, int blk, char buf[ ]);
+int initSuper(int fd);
+SUPER * getSp();
+
+#endif //SHSIMULATOR_DISK_H
